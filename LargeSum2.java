2103368637c59a2ecbb89681e7060b6660824313
Abdulkadir Dilmen <113839940+a-dilmen@users.noreply.github.com>
2022-11-10 00:24:58 +0300

    An alternative with lots of char algebra\_
    Incomplete need some more debugging

---
+++ LargeSum2.java
@@ -0,0 +1,84 @@
+package ProjectEuler;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargeSum2 {
+	public static void main(String[] args) throws IOException {
+		// Large sum
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.nextLine(); // to consume \n remaining from nextInt()
+
+		// Creation of string array to store every input 50 digit number as String
+		String[] stringArray = new String[n];
+
+		// Creation of char[55] to convert strings to char[] and sum on char level
+		// Creation of char[] 5 indices larger to store 50 th and more chars come from
+		// summation input upto 10^3 50 digits #
+		int spareChars = 5;
+		char[] character = new char[50 + spareChars];
+		// initialization of initially empty char[] as (' '-32) which has integer value
+		// of 0
+		for (int i = 0; i < character.length; i++) {
+			character[i] = ' ' - 32;
+		}
+		// receiving and storing 50 digit numbers as string
+		for (int i = 0; i < stringArray.length; i++) {
+			stringArray[i] = scanner.nextLine();
+		}
+
+		// To sum each digit in char level and update char[]
+		// looping for each char
+		for (int j = spareChars; j < character.length; j++) {
+			// looping for String[]
+			for (int i = 0; i < stringArray.length; i++) {
+				// updating char at j by summing integer values of char
+				character[j] += (char) stringArray[i].charAt(j - 5);
+			}
+		}
+		// integer casting of char[]
+		int[] char2int = new int[character.length];
+		for (int i = 0; i < char2int.length; i++) {
+			char2int[i] = (int) character[i];
+		}
+		// converting reminders to digits and updating trailing chars in the char array
+		for (int i = char2int.length - 1; i >= spareChars; i--) {
+			int x = char2int[i] - 48 * n;
+			if (x<=0) {
+				x = 0;
+				continue;
+			}
+			if (x < 100 && x > 9) {
+				char2int[i - 1] = char2int[i - 1] + (x) / 10;
+			} else if (x < 1000) {
+				char2int[i - 2] = char2int[i - 2] + (x) / 100;
+				char2int[i - 1] = char2int[i - 1] + (x - (x / 100) * 100) / 10;
+			} else if (x < 10000) {
+				char2int[i - 3] = char2int[i - 3] + (x) / 1000;
+				char2int[i - 2] = char2int[i - 2] + (x - (x / 1000) * 1000) / 100;
+				char2int[i - 1] = char2int[i - 1] + (x - (x / 1000) * 1000 - (((x % 1000)/100)*100))/10;
+			}
+			char2int[i] = (x % 10);
+		}
+		// string manipulation to write first 10 digits
+		String str = "";
+		int k = 0;
+		for (int i = 0; i < character.length; i++) {
+			if (char2int[i] != 0) {
+				k = i;
+				break;
+			}
+		}
+		for (int j = k; j < char2int.length; j++) {
+			str += char2int[j];
+		}
+		System.out.println(str.substring(0, 10));
+		scanner.close();
+
+	}
+
+}
