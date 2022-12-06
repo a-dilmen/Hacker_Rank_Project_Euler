b44789ebcee2429ee0177a22cf6ad703ef863d1a
Abdulkadir Dilmen <113839940+a-dilmen@users.noreply.github.com>
2022-12-06 09:14:08 +0300

    Smallest Number Java Solution Recursive GCD-LCM

---
+++ SmallestMultiple.java
@@ -0,0 +1,42 @@
+package com.dilmen;
+
+import java.util.Scanner;
+
+public class SmallestMultiple {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			long[] numbers = new long[n];
+			for (int j = 0; j < n; j++) {
+				numbers[j] = j + 1;
+			}
+			System.out.println(LCM(numbers, 0));
+		}
+	}
+
+	public static long gcd(long a, long b) {
+		return b == 0L ? a : gcd(b, a % b);
+
+	}
+
+	public static long lcm(long a, long b) {
+		long lcml = a * b / gcd(a, b);
+		return lcml;
+	}
+
+	private static long LCM(long[] numbers, int i) {
+		try {
+			if (i + 2 == numbers.length) {
+				return lcm(numbers[i], numbers[i + 1]);
+			} else {
+				return lcm(numbers[i], LCM(numbers, i + 1));
+			}
+		} catch (IndexOutOfBoundsException e) {
+
+		}
+		return numbers[0];
+	}
+}
